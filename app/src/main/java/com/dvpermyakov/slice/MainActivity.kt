f88a67cf29c5248a720a55a9ed2a60a9f3233df3
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-07-04 11:07:19 +0400

    init

---
+++ app/src/main/java/com/dvpermyakov/slice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dvpermyakov.slice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
