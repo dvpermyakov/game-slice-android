dbbd5624b76b40385c52a86cb99305a2507bf803
dvpermyakov <dvpermyakov1@gmail.com>
2020-07-10 10:16:20 +0400

    add KodeinFactory.kt

--- app/src/main/java/com/dvpermyakov/slice/MainApplication.kt
+++ app/src/main/java/com/dvpermyakov/slice/MainApplication.kt
@@ -1,34 +1,10 @@
 package com.dvpermyakov.slice
 
 import android.app.Application
-import android.content.res.AssetManager
-import androidx.lifecycle.ViewModelProvider
-import com.dvpermyakov.slice.data.GameRepositoryImpl
-import com.dvpermyakov.slice.domain.GameRepository
-import com.dvpermyakov.slice.screens.game.presentation.GameViewModel
-import com.dvpermyakov.slice.screens.result.presentation.ResultViewModel
-import com.dvpermyakov.slice.utils.ViewModelFactory
+import com.dvpermyakov.slice.di.KodeinFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
-import org.kodein.di.direct
-import org.kodein.di.generic.bind
-import org.kodein.di.generic.instance
-import org.kodein.di.generic.provider
-import org.kodein.di.generic.singleton
 
 class MainApplication : Application(), KodeinAware {
-    override val kodein: Kodein = Kodein.lazy {
-        bind<ViewModelProvider.Factory>() with singleton {
-            ViewModelFactory(kodein.direct)
-        }
-
-        bind<AssetManager>() with singleton { this@MainApplication.applicationContext.assets }
-        bind<GameRepository>() with singleton { GameRepositoryImpl(instance()) }
-        bind<GameViewModel>(tag = GameViewModel::class.java.simpleName) with provider {
-            GameViewModel(instance())
-        }
-        bind<ResultViewModel>(tag = ResultViewModel::class.java.simpleName) with provider {
-            ResultViewModel(instance(), instance())
-        }
-    }
+    override val kodein: Kodein = KodeinFactory.create(this)
 }
---
+++ app/src/main/java/com/dvpermyakov/slice/di/KodeinFactory.kt
@@ -0,0 +1,36 @@
+package com.dvpermyakov.slice.di
+
+import android.app.Application
+import android.content.res.AssetManager
+import androidx.lifecycle.ViewModelProvider
+import com.dvpermyakov.slice.data.GameRepositoryImpl
+import com.dvpermyakov.slice.domain.GameRepository
+import com.dvpermyakov.slice.screens.game.presentation.GameViewModel
+import com.dvpermyakov.slice.screens.result.presentation.ResultViewModel
+import com.dvpermyakov.slice.utils.ViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.direct
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+object KodeinFactory {
+
+    fun create(application: Application): Kodein {
+        return Kodein.lazy {
+            bind<ViewModelProvider.Factory>() with singleton {
+                ViewModelFactory(kodein.direct)
+            }
+            bind<AssetManager>() with singleton { application.applicationContext.assets }
+            bind<GameRepository>() with singleton { GameRepositoryImpl(instance()) }
+            bind<GameViewModel>(tag = GameViewModel::class.java.simpleName) with provider {
+                GameViewModel(instance())
+            }
+            bind<ResultViewModel>(tag = ResultViewModel::class.java.simpleName) with provider {
+                ResultViewModel(instance(), instance())
+            }
+        }
+    }
+
+}
